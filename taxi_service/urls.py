0f1e55bbfb58b8f38d69f3c5ce2ab30bd49ec240
Vova <v.kampanchenko@gmail.com>
2024-10-04 16:21:42 +0300

    Solution3

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,12 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 import debug_toolbar
 
-if settings.DEBUG:
-    urlpatterns = [
-        path("admin/", admin.site.urls),
-        path("taxi/", include("taxi.urls", namespace="taxi")),
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("taxi/", include("taxi.urls", namespace="taxi")),
+    path("__debug__/", include(debug_toolbar.urls)),
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
